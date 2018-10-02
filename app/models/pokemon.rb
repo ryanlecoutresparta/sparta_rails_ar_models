01247b9d9a2e3cd794bc1714f04e3fee8b5d57d1
Tech-A07 <tech-a07@Tech-A07s-MacBook-Air.local>
2018-10-02 22:36:00 +0100

    completed the first iteration of the app

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -4,5 +4,6 @@ class Pokemon < ApplicationRecord
   validates :name, presence: true, length: { in:1..20}
   validates :pokemon_type, presence: true, length: { in:1..8}
   validates :level, presence: true, numericality: { less_than_or_equal_to: 100, greater_than: 0,  only_integer: true }
+  validates :trainer_id, presence: true, numericality: { only_integer: true }
   validates :human, :acceptance => true
 end
